ff5a1859e9c3b7257106fbea8f0a87120b6a5378
Bookkeeper <yarko12@gmail.com>
2024-09-27 17:53:58 +0300

    feature: - add simple page transition - add .env to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
     modules: ['@nuxt/eslint'],
+    app: {
+        pageTransition: { name: 'page', mode: 'out-in' },
+    },
+    runtimeConfig: { public: { apiBase: process.env.API_BASE_URL } },
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
