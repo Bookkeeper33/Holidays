cad96a1a37f280c8570436eef142fed3b47f8076
Bookkeeper <yarko12@gmail.com>
2024-09-27 17:58:08 +0300

    feature: - add dynamic search - fetch all available countries - add dynamic route navigation

---
+++ components/CountryCardComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="card bg-neutral text-neutral-content card-compact">
+        <div class="card-body items-center text-center">
+            <slot />
+        </div>
+    </div>
+</template>
